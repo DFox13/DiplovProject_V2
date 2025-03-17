71a2fc731bd8d50304ca0bd07d9505d9e2a77d62
D.Fox <syz.vadim@gmail.com>
2025-03-17 20:28:43 +0300

    Подключена бд, закинут шаблон

---
+++ laravel-diplom/resources/views/home.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/style/style.css">
+    <link rel="stylesheet" href="fontawesome-free-6.6.0-web/css/all.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    </head>
+<body>
+    
+    <div class="wrap">
+        <div class="header">
+            <div class="logo">
+                <img src="https://www.cstom.ru/wp-content/themes/cstom/images/logo.svg" alt="Логотип">
+            </div>
+            <div class="slogan">
+                <div class="address">
+                    <a href="">г. Смоленск, ул. Кирова, д. 29</a>
+                    <p>по будням с 8:30 до 21:00<br>
+                    в выходные с 9:00 до 18:00</p>
+                </div>
+            </div>
+        </div>
+        <div class="menu-button" id="menuButton">☰ <span id="valuePage"></span>  </div>
+        <div class="nav" id="navbar">
+            <a href="/">Главная</a>
+            <a href="/services">Услуги</a>
+            <a href="/about">О нас</a>
+            <a href="/stock">Акции</a>
+            <a href="/contacts">Контакты</a>
+            <a href="/dentists">Врачи</a>
+            <a href="/reviews">Отзывы</a>
+        </div>
+        <button id="scrollToTop" style="display: none;">Вверх <i class="fa-solid fa-arrow-up"></i></button>
+    
+    <script src="/scripts/scriptHeader.js"></script>
+    <div class="footer">
+            <div class="cont">
+                <img src="https://www.cstom.ru/wp-content/themes/cstom/images/footer_logo.svg">
+                <div class="contacts">
+                    <p><i class="fa-solid fa-phone-volume"></i> 8 (4812) 315-115</br>
+                    <i id="iconWhats" class="fa-brands fa-whatsapp"></i> 8 (910) 710-48-24</p>
+                    <p>Эл. почта: info@cstom.ru</p>
+                    <p>Мы в соцсетях: 
+                    <a href="https://vk.com/smolstom"><i id="iconV" class="fa-brands fa-vk" style="color: #74C0FC;"></i></a> <a href="https://ok.ru/smolstom"><i id="iconOd" class="fa-brands fa-square-odnoklassniki" style="color: #ff6600;"></i></a></p>
+                </div>
+                <div class="addressInFoot">
+                        <a href="">г. Смоленск, ул. Кирова, д. 29</a>
+                        <p>по будням с 8:30 до 21:00</br>
+                        в выходные с 9:00 до 18:00</p>
+                </div>
+                <div class="inf">
+                    <p>ЛО 67-01-000996 от 21 сентября 2015 г. выдана Департаментом Смоленской области по здравоохранению.</p>
+                    <p>ООО «ЦСС», с 2014 года.</p>
+                </div>
+            </div>
+            <div class="createBy">Created by V.Zhelanov</div>
+        </div>
+    </div>
+</body>
+</html>
