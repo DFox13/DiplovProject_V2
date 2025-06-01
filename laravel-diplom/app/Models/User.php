35413c541601dd66dd6a0f9b066fd66b624e34d1
D.Fox <syz.vadim@gmail.com>
2025-06-01 17:16:57 +0300

    Обработка на уровень доступа обычнго пользователя

--- laravel-diplom/app/Models/User.php
+++ laravel-diplom/app/Models/User.php
@@ -26,6 +26,11 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class);
     }
+    
+    public function isUser(): bool
+    {
+        return $this->role && $this->role->name === 'user';
+    }
 
     public function isAdmin(): bool
     {
